7816955b1aa169ecf5addb67bd7c67e307e624bf
macmaad <rod.mac.mad@gmail.com>
2020-11-30 19:44:23 -0600

    initial commit mvp mockup

---
+++ sudoku_utils.py
@@ -0,0 +1,26 @@
+class Tree:
+
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+
+def check_if_valid_in_row(game_grid):
+    ...
+
+
+def check_if_valid_in_column(game_grid):
+    ...
+
+
+def check_if_valid_in_3_3_sub_matrix(game_grid):
+    ...
+
+
+def get_next_empty_space(game_grid):
+    ...
+
+
+def get_possible_values_for_space(row, column):
+    ...
+
